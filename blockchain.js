5fd1d00bf4319fdec472f2a926180f99494dd885
Ankit Salian <ankitsalian9@gmail.com>
2021-04-14 07:14:49 +0530

    Added code for isValidChain to satisfy the test suite

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain {
 
@@ -13,6 +14,30 @@ class Blockchain {
         });
 
         this.chain.push(newBlock);
+    }
+
+    static isValidChain(chain) {
+
+        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            return false;
+        }
+
+        for(let i = 1; i < chain.length; i++) {
+            const {timestamp, data, hash, lastHash} = chain[i];
+            const actualLastHashValue = chain[i - 1].hash;
+
+            if(lastHash !== actualLastHashValue) {
+                return false;
+            }
+
+            const validatedHash = cryptoHash(timestamp, data, lastHash);
+
+            if(hash !== validatedHash) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
