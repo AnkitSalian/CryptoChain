c1d7d870e8e4e0755d555e0145ee3a8e2712e6a9
Ankit Salian <ankitsalian9@gmail.com>
2021-04-15 09:09:29 +0530

    Added test suite to implement proof of work by adding variable nonce and difficulty

--- block.test.js
+++ block.test.js
@@ -7,14 +7,18 @@ describe('Block', () => {
     const lastHash = 'foo-lastHash';
     const data = 'foo-data';
     const hash = 'foo-hash';
+    const nonce = 1;
+    const difficulty = 1;
 
-    const block = new Block({ timestamp, lastHash, data, hash });
+    const block = new Block({ timestamp, lastHash, data, hash, nonce, difficulty });
 
     it('has timestamp, lastHash, hash and hash', () => {
         expect(block.timestamp).toEqual(timestamp);
         expect(block.lastHash).toEqual(lastHash);
         expect(block.data).toEqual(data);
         expect(block.hash).toEqual(hash);
+        expect(block.nonce).toEqual(nonce);
+        expect(block.difficulty).toEqual(difficulty);
     });
 
     describe('genesis()', () => {
@@ -51,8 +55,18 @@ describe('Block', () => {
         });
 
         it('creates a SHA-256 `hash` based on proper inputs', () => {
-            expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
-        })
+            expect(minedBlock.hash).toEqual(
+                cryptoHash(minedBlock.timestamp,
+                    minedBlock.nonce,
+                    minedBlock.difficulty,
+                    lastBlock.hash,
+                    data)
+            );
+        });
+
+        it('sets ann `hash` that meets difficulty criteria', () => {
+            expect(minedBlock.hash.substring(0, minedBlock.difficulty)).toEqual('0'.repeat(minedBlock.difficulty));
+        });
     })
 
 })
