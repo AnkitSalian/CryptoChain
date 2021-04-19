e0071494d60e5fc0d02a98552676ec7c6406a014
Ankit Salian <ankitsalian9@gmail.com>
2021-04-19 08:24:32 +0530

    Check average time for adding a block while checking for difficulty

---
+++ average-work.js
@@ -0,0 +1,25 @@
+const Blockchain = require('./blockchain');
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock({ data: 'initialData' });
+
+let prevTimestamp, nextTimeStamp, nextBlock, timeDiff, average;
+
+const times = [];
+
+for (let i = 0; i < 10000; i++) {
+    prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
+
+    blockchain.addBlock(`initial ${i}`);
+
+    nextBlock = blockchain.chain[blockchain.chain.length - 1];
+
+    nextTimeStamp = nextBlock.timestamp;
+    timeDiff = nextTimeStamp - prevTimestamp;
+    times.push(timeDiff);
+
+    average = times.reduce((total, num) => (total + num)) / times.length;
+
+    console.log(`Time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`);
+}
